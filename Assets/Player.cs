e92acce69679dcbfb28e057bbf7cea1504e4bffe
Phil Davidson <pxdavidson@msn.com>
2019-07-20 01:03:56 +0100

    Uploading first pass
    Got the move and jump working

---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Variables
+    [SerializeField] float moveSpeed = 1;
+    [SerializeField] float jumpForce = 1;
+    Rigidbody rigidbody;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
